ec90d19b8c65fb64db69a9eafe8020937fb8ba40
pmork7 <paul.a.mork@gmail.com>
2020-06-02 18:28:40 +0800

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0
+with open("Data\portfolio.csv","r") as f:
+    next(f)
+    for line in f:
+        line = line.replace("\n","")
+        items = line.split(",")
+        total_cost += float(items[1]) * float(items[2])
+print(f"Total cost is ${total_cost}")
