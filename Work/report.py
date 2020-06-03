4c26093604c2b249031cedde0dc140590f7806a3
pmork7 <paul.a.mork@gmail.com>
2020-06-03 17:41:54 +0800

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,28 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+# report.py
+#
+# Exercise 2.4
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename,"r") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                holding = (row[0], int(row[1]), float(row[2]))
+                portfolio.append(holding)
+            except ValueError:
+                print("Couldn't parse", row)
+    return portfolio
+
+filename = ""
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+result = read_portfolio(filename)
+print(result)
