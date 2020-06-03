a8193e921c0d4727d909bb93064fed2378f03ae4
pmork7 <paul.a.mork@gmail.com>
2020-06-03 17:20:31 +0800

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,25 @@
+import csv
+import sys
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0
-with open("Data\portfolio.csv","r") as f:
-    next(f)
-    for line in f:
-        line = line.replace("\n","")
-        items = line.split(",")
-        total_cost += float(items[1]) * float(items[2])
-print(f"Total cost is ${total_cost}")
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename,"r") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                a = row
+                total_cost += float(a[1]) * float(a[2])
+            except ValueError:
+                print("Couldn't parse", row)
+    return total_cost
+
+filename = ""
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+cost = portfolio_cost(filename)
+print(f"Total cost is ${cost}")
